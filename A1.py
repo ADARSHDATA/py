5b8c62c4b99ec4dd56a45d5d7da660a6abe4163c
adarsh sharma <adarshsharma58800@gmail.com>
2026-08-14 10:56:28 +0530

    assignment 1 added

---
+++ A1.py
@@ -0,0 +1,80 @@
+"""Q1
+
+Add two numbers A and B.
+
+A, B = input().split()
+A = int(A)
+B = int(B)
+print(A + B)
+Q2"""
+"""
+Check if A is less than 10 and greater than B.
+
+A, B = input().split()
+A = int(A)
+B = int(B)
+
+
+if A < 10 and A > B:
+    print(True)
+else:
+    print(False)"""
+"""Q3
+
+Check if at least one number is divisible by 10.
+
+A, B = input().split()
+A = int(A)
+B = int(B)
+
+
+if A % 10 == 0 or B % 10 == 0:
+    print("true")
+else:
+    print("false")"""
+"""Q4
+
+Find the first digit of a 4-digit number.
+
+N = int(input())
+print(N // 1000)
+Q5
+
+Find the last digit of a 4-digit number.
+
+N = int(input())
+print(N % 10)
+Q6
+
+Find the remainder when B is divided by A.
+
+A, B = input().split()
+A = int(A)
+B = int(B)
+
+print(B % A)"""
+"""Q7
+
+Multiply two numbers A and B.
+
+A, B = input().split()
+A = int(A)
+B = int(B)
+
+
+print(A * B)
+Q8
+
+Find the total and average of three marks.
+
+A, B, C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+
+
+total = A + B + C
+average = total / 3
+
+
+print(total, round(average, 2))"""
